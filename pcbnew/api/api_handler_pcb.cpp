c776e086b7a848d876e24472854b9ac33b1c2c1f
Jon Evans <jon@craftyjon.com>
2026-03-23 21:18:01 -0400

    API: Add support for barcodes and reference images

--- pcbnew/api/api_handler_pcb.cpp
+++ pcbnew/api/api_handler_pcb.cpp
@@ -709,6 +709,7 @@ HANDLER_RESULT<GetItemsResponse> API_HANDLER_PCB::handleGetItems( const HANDLER_
         case PCB_TEXT_T:
         case PCB_TEXTBOX_T:
         case PCB_BARCODE_T:
+        case PCB_REFERENCE_IMAGE_T:
         {
             handledAnything = true;
             bool inserted = false;
@@ -727,7 +728,7 @@ HANDLER_RESULT<GetItemsResponse> API_HANDLER_PCB::handleGetItems( const HANDLER_
 
             break;
         }
-        
+
         case PCB_DIMENSION_T:
         {
             handledAnything = true;
